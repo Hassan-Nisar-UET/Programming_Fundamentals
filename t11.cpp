f683ab3daee1e90270ce7f16994fabea05d70e35
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-11 05:32:00 +0500

    Add files via upload

---
+++ t11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    cout<<"**************************"<<endl;
+    cout<<"   EXAMPLE WORK DONE      "<<endl;
+    cout<<"**************************"<<endl;
+    float force;
+    float disp;
+    float workDone;
+    cout<<"enter force"<<endl;
+    cin>>force;
+    cout<<"enter diplacement"<<endl;
+    cin>>disp;
+    workDone=force*disp;
+    cout<<"the work done is"<<"   "<<workDone<<endl;
+
+
+    
+    
+}

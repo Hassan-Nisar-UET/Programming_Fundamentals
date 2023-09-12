06e4c53237916b25dc690f38be4f84a46dc988eb
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-12 14:42:02 +0500

    Add files via upload

---
+++ t12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"**************************"<<endl;
+    cout<<"   EXAMPLE momentum       "<<endl;
+    cout<<"**************************"<<endl;
+    float mass;
+    float velocity;
+    float momentum;
+    cout<<"enter mass"<<endl;
+    cin>>mass;
+    cout<<"enter velocity"<<endl;
+    cin>>velocity;
+    momentum=mass*velocity;
+    cout<<"the required momentum is"<<"  "<<momentum<<endl;
+    
+}

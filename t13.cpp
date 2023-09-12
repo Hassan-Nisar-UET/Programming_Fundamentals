69dd9983b256dd2cff26d27129fa2485917c1ada
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-12 14:42:27 +0500

    Add files via upload

---
+++ t13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"**************************"<<endl;
+    cout<<"   EXAMPLE 11.4           "<<endl;
+    cout<<"**************************"<<endl;
+    float result;
+    int temperature1;
+    cout<<"enter temprature1"<<endl;
+    cin>>temperature1;
+    int temperature2;
+    cout<<"enter temperature2"<<endl;
+    cin>>temperature2;
+    result=temperature1-temperature2;
+    result=result/temperature1;
+    cout<<"result is"<<"   "<<result<<endl;
+
+
+}

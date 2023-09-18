fd8e0ae69e94655f2491b81b62699ebba3cb1cd5
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-18 11:42:24 -0700

    Add files via upload

---
+++ checkVOvel.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch='a';
+    char ca='e';
+    char cb='e';
+    char cc='o';
+    char cd='u';
+    char cha;
+    cout<<"enter your character"<<endl;
+    cin>>cha;
+    if(cha == ch){
+        cout<<"your character is voval"<<endl;
+
+    }
+    else if(cha == ca){
+        cout<<"your character is voval"<<endl;
+
+    }
+    else if(cha == cb){
+        cout<<"your character is voval"<<endl;
+
+    }
+    else if(cha == cc){
+        cout<<"your character is voval"<<endl;
+
+    }
+    else if(cha == cd){
+        cout<<"your character is voval"<<endl;
+
+    }
+    else{
+        cout<<"your character is not voval"<<endl;
+    }
+    
+}

44528ad158a88bc4def703da734cccdbf3dd27b7
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-19 18:17:09 -0700

    Add files via upload

---
+++ counting.cpp
@@ -0,0 +1,307 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter your number"<<endl;
+    cin>>number;
+    if(number == 1){
+        cout<<"one"<<endl;
+    }
+    else if(number == 2){
+        cout<<"two"<<endl;
+    }
+    else if(number == 3){
+        cout<<"three"<<endl;
+    }
+    else if(number == 4){
+        cout<<"four"<<endl;
+    }
+    else if(number == 5){
+        cout<<"five"<<endl;
+    }
+    else if(number == 6){
+        cout<<"six"<<endl;
+    }
+    else if(number == 7){
+        cout<<"seven"<<endl;
+    }
+    else if(number == 8){
+        cout<<"eight"<<endl;
+    }
+    else if(number == 9){
+        cout<<"nine"<<endl;
+    }
+    else if(number == 10){
+        cout<<"ten"<<endl;
+    }
+    else if(number == 11){
+        cout<<"eleven"<<endl;
+    }
+    else if(number == 12){
+        cout<<"twelve"<<endl;
+    }
+    else if(number == 13){
+        cout<<" thirteen"<<endl;
+    }
+    else if(number == 14){
+        cout<<"fourteen"<<endl;
+    }
+    else if(number == 15){
+        cout<<"fifteen"<<endl;
+    }
+    else if(number == 16){
+        cout<<"sixteen"<<endl;
+    }
+    else if(number == 17){
+        cout<<"seventeen"<<endl;
+    }
+    else if(number == 18){
+        cout<<"eighteen"<<endl;
+    }
+    else if(number == 19){
+        cout<<"nineteen"<<endl;
+    }
+    else if(number == 20){
+        cout<<"twenty"<<endl;
+    }
+    else if(number == 21){
+        cout<<" twenty-one"<<endl;
+    }
+    else if(number == 22){
+        cout<<"twenty-two"<<endl;
+    }
+    else if(number == 23){
+        cout<<"twenty-three"<<endl;
+    }
+    else if(number == 24){
+        cout<<"twenty-four"<<endl;
+    }
+    else if(number == 25){
+        cout<<"twenty-five"<<endl;
+    }
+    else if(number == 26){
+        cout<<" twenty-six"<<endl;
+    }
+    else if(number == 27){
+        cout<<"twenty-seven"<<endl;
+    }
+    else if(number == 28){
+        cout<<"twenty-eight"<<endl;
+    }
+    else if(number == 29){
+        cout<<"twenty-nine"<<endl;
+    }
+    else if(number == 30){
+        cout<<"thirty"<<endl;
+    }
+    else if(number == 31){
+        cout<<"31 – thirty-one"<<endl;
+    }
+    else if(number == 32){
+        cout<<"32 – thirty-two"<<endl;
+    }
+    else if(number == 33){
+        cout<<"33 – thirty-three"<<endl;
+    }
+    else if(number == 34){
+        cout<<"34 – thirty-four"<<endl;
+    }
+    else if(number == 35){
+        cout<<"35 – thirty-five"<<endl;
+    }
+    else if(number == 36){
+        cout<<"36 – thirty-six"<<endl;
+    }
+    else if(number == 37){
+        cout<<"37 – thirty-seven"<<endl;
+    }
+    else if(number == 38){
+        cout<<"38 – thirty-eight"<<endl;
+    }
+    else if(number == 39){
+        cout<<"39 – thirty-nine"<<endl;
+    }
+    else if(number == 40){
+        cout<<"40 – forty"<<endl;
+    }
+    else if(number == 41){
+        cout<<"41 – forty-one"<<endl;
+    }
+    else if(number == 42){
+        cout<<"42 – forty-two"<<endl;
+    }
+    else if(number == 43){
+        cout<<"43 – forty-three"<<endl;
+    }
+    else if(number == 44){
+        cout<<"44 – forty-four"<<endl;
+    }
+    else if(number == 45){
+        cout<<"45 – forty-five"<<endl;
+    }
+    else if(number == 46){
+        cout<<"46 – forty-six"<<endl;
+    }
+    else if(number == 47){
+        cout<<"47 – forty-seven"<<endl;
+    }
+    else if(number == 48){
+        cout<<"48 – forty-eight"<<endl;
+    }
+    else if(number == 49){
+        cout<<"49 – forty-nine"<<endl;
+    }
+    else if(number == 50){
+        cout<<"50 – fifty"<<endl;
+    }
+    else if(number == 51){
+        cout<<"51 – fifty-one"<<endl;
+    }
+    else if(number == 52){
+        cout<<"52 – fifty-two"<<endl;
+    }
+    else if(number == 53){
+        cout<<"53 – fifty-three"<<endl;
+    }
+    else if(number == 54){
+        cout<<"54 – fifty-four"<<endl;
+    }
+    else if(number == 55){
+        cout<<"55 – fifty-five"<<endl;
+    }
+    else if(number == 56){
+        cout<<"56 – fifty-six"<<endl;
+    }
+    else if(number == 57){
+        cout<<"57 – fifty-seven"<<endl;
+    }
+    else if(number == 58){
+        cout<<"58 – fifty-eight"<<endl;
+    }
+    else if(number == 59){
+        cout<<"59 – fifty-nine"<<endl;
+    }
+    else if(number == 60){
+        cout<<"60 – sixty"<<endl;
+    }
+    else if(number == 61){
+        cout<<"61 – sixty-one"<<endl;
+    }
+    else if(number == 62){
+        cout<<"62 – sixty-two"<<endl;
+    }
+    else if(number == 63){
+        cout<<"63 – sixty-three"<<endl;
+    }
+    else if(number == 64){
+        cout<<"64 – sixty-four"<<endl;
+    }
+    else if(number == 65){
+        cout<<"65 – sixty-five"<<endl;
+    }
+    else if(number == 66){
+        cout<<"66 – sixty-six"<<endl;
+    }
+    else if(number == 67){
+        cout<<"67 – sixty-seven"<<endl;
+    }
+    else if(number == 68){
+        cout<<"68 – sixty-eight"<<endl;
+    }
+    else if(number == 69){
+        cout<<"69 – sixty-nine"<<endl;
+    }
+    else if(number == 70){
+        cout<<"70 – seventy"<<endl;
+    }
+    else if(number == 71){
+        cout<<"71 – seventy-one"<<endl;
+    }
+    else if(number == 72){
+        cout<<"72 – seventy-two"<<endl;
+    }
+    else if(number == 73){
+        cout<<"73 – seventy-three"<<endl;
+    }
+    else if(number == 74){
+        cout<<"74 – seventy-four"<<endl;
+    }
+    else if(number == 75){
+        cout<<"75 – seventy-five"<<endl;
+    }
+    else if(number == 76){
+        cout<<"76 – seventy-six"<<endl;
+    }
+    else if(number == 77){
+        cout<<"77 – seventy-seven"<<endl;
+    }
+    else if(number == 78){
+        cout<<"78 – seventy-eight"<<endl;
+    }
+    else if(number == 79){
+        cout<<"79 – seventy-nine"<<endl;
+    }
+    else if(number == 80){
+        cout<<"80 – eighty"<<endl;
+    }
+    else if(number == 81){
+        cout<<"81 – eighty-one"<<endl;
+    }
+    else if(number == 82){
+        cout<<"82 – eighty-two"<<endl;
+    }
+    else if(number == 83){
+        cout<<"83 – eighty-three"<<endl;
+    }
+    else if(number == 84){
+        cout<<"84 – eighty-four"<<endl;
+    }
+    else if(number == 85){
+        cout<<"85 – eighty-five"<<endl;
+    }
+    else if(number == 86){
+        cout<<"86 – eighty-six"<<endl;
+    }
+    else if(number == 87){
+        cout<<"87 – eighty-seven"<<endl;
+    }
+    else if(number == 88){
+        cout<<"88 – eighty-eight"<<endl;
+    }
+    else if(number == 89){
+        cout<<"89 – eighty-nine"<<endl;
+    }
+    else if(number == 90){
+        cout<<"90 – ninety"<<endl;
+    }
+    else if(number == 91){
+        cout<<"91 – ninety-one"<<endl;
+    }
+    else if(number == 92){
+        cout<<"92 – ninety-two"<<endl;
+    }
+    else if(number == 93){
+        cout<<"93 – ninety-three"<<endl;
+    }
+    else if(number == 94){
+        cout<<"94 – ninety-four"<<endl;
+    }
+    else if(number == 95){
+        cout<<"95 – ninety-five"<<endl;
+    }
+    else if(number == 96){
+        cout<<"96 – ninety-six"<<endl;
+    }
+    else if(number == 97){
+        cout<<"97 – ninety-seven"<<endl;
+    }
+    else if(number == 98){
+        cout<<"98 – ninety-eight"<<endl;
+    }
+    else if(number == 99){
+        cout<<"99 – ninety-nine"<<endl;
+    }
+    else if(number == 100){
+        cout<<"100 – one hundred *"<<endl;
+    }
+}

2268e53c87a47b845e3cf66a3ac77dbaf7a4987a
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-19 18:17:31 -0700

    Add files via upload

---
+++ drawPattren.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+     cout<<"ccccccccccccc"<<"          "<<"\t"<<"++"<<"\t"<<"          "<<"\t"<<"++"<<"\t"<<endl;
+     cout<<"cc"<<"                     "<<"++++++++++++++"<<"          "<<"++++++++++++++"<<endl;
+     cout<<"cc"<<"                     "<<"++++++++++++++"<<"          "<<"++++++++++++++"<<endl;
+     cout<<"cc"<<"                     "<<"\t"<<"++"<<"\t"<<"          "<<"\t"<<"++"<<"\t"<<endl;
+     cout<<"cc"<<endl;
+     cout<<"ccccccccccccc"<<endl;
+
+}

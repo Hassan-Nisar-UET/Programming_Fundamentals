37dce16b7dfbfc371552be3548291cce50084e7c
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-21 10:45:07 -0700

    Add files via upload

---
+++ fuleGallons.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float totalMiles;
+    int gallons;
+    cout<<"enter numbers of gallon"<<endl;
+    cin>>gallons;
+    float miles;
+    cout<<"enter mils"<<endl;
+    cin>>miles;
+    totalMiles=miles*gallons;
+    cout<<"total number of miles are.."<<totalMiles<<endl;
+
+
+}

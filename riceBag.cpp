182e662abfe50dc2a7b04805be39a6a39bf75704
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-09-25 23:03:18 +0500

    Add files via upload

---
+++ riceBag.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int pounds;
+    cout<<"enter the number of pound in each rice bag"<<endl;
+    cin>>pounds;
+    int store;
+    store=2205/pounds;
+    cout<<store<<".. bag are required for rice"<<endl;
+
+}

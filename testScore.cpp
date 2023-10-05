3c719874d0cadbb1daa5b85907d1891fe9a82dd4
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-10-05 14:29:23 +0500

    Add files via upload

---
+++ testScore.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float totalAvg;
+    float score1;
+    cout<<"enter score1"<<endl;
+    cin>>score1;
+    float score2;
+    cout<<"enter score2"<<endl;
+    cin>>score2;
+    float score3;
+    cout<<"enter score3"<<endl;
+    cin>>score3;
+    float score4;
+    cout<<"enter score4"<<endl;
+    cin>>score4;
+    float score5;
+    cout<<"enter score5"<<endl;
+    cin>>score5;
+    totalAvg=(score1+score2+score3+score4+score5)/5;
+    cout<<"the total average of test score is"<<totalAvg<<endl;
+}

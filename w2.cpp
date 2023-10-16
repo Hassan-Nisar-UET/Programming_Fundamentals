c104d2f3039989b2f1f49f7dfe7010dd1d14c532
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-10-17 01:04:49 +0500

    Add files via upload

---
+++ w2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"************************"<<endl;
+    cout<<"     EXAMLPE  1.2       "<<endl;
+    cout<<"************************"<<endl;
+    float mass=0;
+    float mass1=0;
+    float mass2=0;
+    float totalMass;
+    cout<<"enter mass"<<endl;
+    cin>>mass;
+    cout<<"enter mass1"<<endl;
+    cin>>mass1;
+    cout<<"enter mass2"<<endl;
+    cin>>mass2;
+    totalMass=mass+mass1+mass2;
+    cout<<"total mass is"<<"  "<<totalMass<<endl;
+}

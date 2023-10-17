307a9a3e45d73b27a6152afdb0060fe239436fd7
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-10-18 02:09:53 +0500

    Add files via upload

---
+++ w4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    cout<<"************************"<<endl;
+    cout<<"     EXAMPLE  2.5       "<<endl;
+    cout<<"************************"<<endl;
+    float A;
+    float B;
+    float C;
+    cout<<"enter A "<<endl;
+    cin>>A;
+    cout<<"enter B"<<endl;
+    cin>>B;
+    C=A*B;
+    B=sqrt(B);
+    C=C/B;
+    cout<<"the value of c is"<<"  "<<C<<endl;
+
+}

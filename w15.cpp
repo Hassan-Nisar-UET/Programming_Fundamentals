ce6916286e4cfea94700307988b439e8d84aa768
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-11-14 20:14:57 +0500

    Add files via upload

---
+++ w15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"**************************"<<endl;
+    cout<<"   EXAMPLE 2.4          "<<endl;
+    cout<<"**************************"<<endl;
+    float result;
+    float force;
+    cout<<"enter force"<<endl;
+    cin>>force;
+    float d;
+    cout<<"enter value od d"<<endl;
+    cin>>d;
+    result=force*d;
+    cout<<"total work id"<<"  "<<result<<endl;
+}

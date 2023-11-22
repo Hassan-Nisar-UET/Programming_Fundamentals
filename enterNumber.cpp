067f98ba943c83166ddc0cd3487e02d652821b3c
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-11-22 18:19:59 +0500

    Add files via upload

---
+++ enterNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int counter=0;
+    int number;
+    cout<<"enter number "<<endl;
+    cin>>number;
+    while (number != counter)
+    {
+        cout<<"enter number"<<endl;
+        cin>>counter;
+    }
+    
+   
+}

93c229a66d9ab0c7cfa36c7421874fb05d60dd16
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-11-23 14:32:49 +0500

    Add files via upload

---
+++ febonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1,n2,n3,count;
+    cout<<"enter n1"<<endl;
+    cin>>n1;
+    cout<<"enter n2"<<endl;
+    cin>>n2;
+    cout<<"enter n3"<<endl;
+    cin>>n3;
+    for(int i=0;i<=n3;i++){
+        count=n1+n2;
+    }
+
+}

0543a695111cafa4a22817b9045f048e92b2bc0b
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-11-27 19:20:17 +0500

    Add files via upload

---
+++ multiples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int count=0;
+    for(int i=1; i<=100 ;i++){
+        count=13*i;
+        if(count <=1000){
+        cout<<count<<endl;
+        }
+        
+    }
+}

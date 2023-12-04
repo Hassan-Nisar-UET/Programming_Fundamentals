34bc75889680829cb30bb1dbfcde55420b9adad1
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-12-04 11:57:33 +0500

    Add files via upload

---
+++ multiple_w.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int count=1;
+    int number;
+    while(number < 1000){
+         number=13*count;
+         cout<<number<<endl;
+         count++;
+    }
+}

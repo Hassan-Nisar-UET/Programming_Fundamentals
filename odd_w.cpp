294db60302d6f18907af1617bbfcc22a0877ad34
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-12-05 07:39:35 +0500

    Add files via upload

---
+++ odd_w.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int counter=1;
+    while(counter != 11){
+        if(counter%2==1){
+            cout<<counter<<endl;
+            
+        }
+        counter++;
+    }
+}

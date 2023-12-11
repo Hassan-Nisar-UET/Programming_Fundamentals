7b4a42a7620bf25307dc1d68709def850606d53a
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-12-11 12:59:39 +0500

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int number;
+ cout<<"enter the number"<<endl;
+ cin>>number;
+ for(int i=1 ; i<=10 ; i++ ){
+     cout<<number<<" x "<<i<<"="<<number*i<<endl;
+ }
+    
+}

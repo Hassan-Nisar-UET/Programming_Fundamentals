582d6da96c5f0279856adb74df37ee14ae98164d
Hassan Nisar <93261648+Hassan-Nisar-UET@users.noreply.github.com>
2023-12-16 00:00:36 +0500

    Add files via upload

---
+++ upperCase.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+int main(){
+    char letter;
+    
+    while(letter != '#'){
+        cout<<"enter the letter"<<endl;
+        cin>>letter;
+          if(letter == 'a' || letter == 'b' || letter == 'c'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..2"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+              
+          }
+          else if(letter == 'd' || letter == 'e' || letter == 'f'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..3"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+          }
+         else if(letter == 'g' || letter == 'h' || letter == 'i'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..4"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+          }
+          else if(letter == 'j' || letter == 'k' || letter == 'l'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..5"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+          }
+          else if(letter == 'm' || letter == 'n' || letter == 'o'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..6"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+          }
+           else if(letter == 'p' || letter == 'q' || letter == 'r' || letter == 's'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..7"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+          }
+           else if(letter == 't' || letter == 'u' || letter == 'v'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..8"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+          }
+            else if(letter == 'w' || letter == 'x' || letter == 'y' || letter == 'z'){
+              cout<<"your enter letter is.."<<letter<<endl;
+              cout<<"the corresponding telephone digit is..9"<<endl;
+              cout<<"press any key to countinue"<<endl;
+              getch();
+          }
+          
+    }
+    
+}
